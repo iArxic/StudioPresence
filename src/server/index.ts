52c6f43a4ace5bb6a1255c3805450290f0010d0a
arxy <77511250+iArxic@users.noreply.github.com>
2022-12-22 12:40:00 +0100

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -52,7 +52,7 @@ async function main() {
           });
 
           if (data.updateType === "CLOSE") {
-            drpcClient.Destroy()
+            drpcClient.destroy()
           }
  
           res.writeHead(200, { "Content-Type": "text/plain" });
